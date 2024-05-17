9d903562c26bfa2462b130d0ff46a1f4f09e5184
5p4c351ck <kntazanis@gmail.com>
2024-05-17 23:38:38 +0300

    Getopt

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <getopt.h>
+
 #include "grid.h"
 #include "rendering.h"
 
@@ -7,11 +9,63 @@ static int dead_cells = 0;
 static int current = 0;
 static int next = 1;
 
+static char filename[20];
+
 struct snapshot s;
 unsigned long delay = 100;
 
 int main(int argc, char* argv[]){
 	
+	int opt;
+    int option_index = 0;
+
+	static struct option long_options[] = {
+        {"help", no_argument, 0, 'h'},
+        {"load", required_argument, 0, 'l'},
+		{"rule", required_argument, 0, 'r'},
+        {"output", required_argument, 0, 'o'},
+        {0, 0, 0, 0}
+    };
+
+	 while ((opt = getopt_long(argc, argv, "hl:pr:o:", long_options, &option_index)) != -1) {
+        switch (opt) {
+            case 'h':
+                printf("Usage: %s [options]\n", argv[0]);
+                printf("Options:\n");
+                printf("  -h, --help\t\tDisplay this help message\n");
+                printf("  -l, --load\t\tLoad a snapshot to resume from\n");
+				printf("  -p, --print\t\tPrint a list of standard rules\n");
+				printf("  -r, --rule\t\tSpecify a standard rule eg. conway\n");
+                printf("  -o, --output=FILE\tSpecify output file(snapshot) name in case you create one\n");
+                exit(EXIT_SUCCESS);
+            case 'l':
+                printf("snapshot loaded\n");
+                exit(EXIT_SUCCESS);
+			case 'p':
+                printf("Rule list:\n");
+				printf("conway\n");
+                exit(EXIT_SUCCESS);
+			case 'r':
+                printf("Standard rule set: %s\n", optarg);
+                exit(EXIT_SUCCESS);
+            case 'o':
+				/*Add buffer overflow check*/
+				strcpy(filename, optarg);
+                printf("Output file name set: %s\n", filename);
+                break;
+            case '?':
+                /*Handle unknown arguments*/
+                fprintf(stderr, "Invalid option: %c\n", optopt);
+                exit(EXIT_FAILURE);
+            default:
+                exit(EXIT_FAILURE);
+        }
+    }
+
+	for (; optind < argc; optind++) {
+        printf("Non-option argument: %s\n", argv[optind]);
+    }
+
 	struct SDL_info sdlInfo;
 
 	initialization(&sdlInfo);
@@ -41,8 +95,7 @@ int main(int argc, char* argv[]){
 							s.delay = delay;
 							s.iterations = iterations;
 							copy_grid(grid, s.grid, current);
-
-							char filename[20];
+							/*Add if filename empty logic*/
 							strftime(filename, sizeof(filename), "%Y-%m-%d", localtime(&s.time_of_snapshot));
     						FILE *file = fopen(filename, "wb");
     						if (file == NULL) {
